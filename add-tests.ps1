9fa39a3d81535a493b40fa826da02a2bbdf40775
Anatoly Cherenkov <anatoly.cherenkov@gmail.com>
2024-01-04 11:50:37 +0100

    add compose-tes

--- add-tests.ps1
+++
@@ -1,5 +0,0 @@
-$testCount = 20
-foreach ($i in 1..$testCount) {
-    Write-Output "##teamcity[testStarted flowId='$i' name='test.B.$i']"
-    Write-Output "##teamcity[testFinished flowId='$i' name='test.B.$i']"
-}
